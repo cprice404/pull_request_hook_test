115c3a0b6ff49b72513bb13b240b334a13a67867
Chris Price <chris@puppetlabs.com>
2012-02-18 12:57:58 -0800

    Pretty print JSON payload if one is found

--- bin/dinky_echo_server.rb
+++ bin/dinky_echo_server.rb
@@ -2,6 +2,7 @@ require 'socket'
 require 'webrick/httprequest'
 require 'webrick/httpresponse'
 require 'webrick/config'
+require 'json'
 
 def log(msg)
   puts(msg)
@@ -12,10 +13,15 @@ loop {
   client = server.accept
   request = WEBrick::HTTPRequest.new(WEBrick::Config::HTTP)
   request.parse(client)
+
+  payload = ""
+  if (request.query.has_key?("payload")) then
+    payload =  "\n\nPAYLOAD:\n" + JSON.pretty_generate(JSON.parse(request.query["payload"]))
+  end
   log_msg = <<-RESPONSE
 REQUEST RECEIVED:
 -----------------------------------------------------------------------
-#{request.to_s.strip}
+#{request.to_s.strip}#{payload}
 -----------------------------------------------------------------------
   RESPONSE
 
